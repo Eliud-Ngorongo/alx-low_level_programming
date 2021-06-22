a96b3cb16a5413fa120ee22f026793999370dd0f
Eliud-Ngorongo <eliudnjenga@gmail.com>
2021-06-22 16:01:58 +0300

    6Th task

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ *_abs - computes the absolute value of an interger
+ *@n: The interger to check
+ *Return: the absolute value of the interger
+ */
+int _abs(int n)
+{
+if (n >= 0)
+{
+return (n);
+}
+return (-n);
+}
