1e04968f8556bb75f2f9ba93350ce92e3ec3f002
Eliud-Ngorongo <eliudnjenga@gmail.com>
2021-06-22 20:13:34 +0300

    10th task

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ *add - adds two intergers and returns the sum
+ *@a:int to be added to b
+ *@b:int to be added to a
+ *
+ *Return:sum of a and b
+ */
+int add(int a, int b)
+{
+int c;
+c = a + b;
+return (c);
+}
