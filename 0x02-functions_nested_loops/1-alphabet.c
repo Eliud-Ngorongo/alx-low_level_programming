cf90bfd239c13ddaf3b37436c185e26074954190
Eliud-Ngorongo <eliudnjenga@gmail.com>
2021-06-22 13:28:33 +0300

    Second task

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * main - check the code for ALX School students.
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+  char b;
+for (b='a';b<='z';b++)
+{
+_putchar(b);
+}
+
+}
