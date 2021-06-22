3f57e579fb2c0be52e6bc121f2f00e9d2afb0a69
Eliud-Ngorongo <eliudnjenga@gmail.com>
2021-06-22 14:47:42 +0300

    4th task

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * _isalpha- checks for characters whether lower or uppercase
+ *@c:character to check
+ *Return: 1 if c is lowercase or uppercase, 0 otherwise
+ */
+int _isalpha(int c)
+{
+if (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z')
+return (1);
+else
+return (0);
+}
