80175960db243a73066813e9f3a1671f04f41419
Eliud-Ngorongo <eliudnjenga@gmail.com>
2021-06-24 19:00:19 +0300

    Fizzzzzz Buzzzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,30 +1,33 @@
 #include <stdio.h>
 #include "holberton.h"
+
 /**
-* main - prints Fizz for int divisible by 3 and Buzz for 5
-*
-* Return: int
-*/
+ * main - entry point
+ *
+ * Description: prints the numbers 1 - 100 w/
+ * Fizz for multiples of 3, Buzz for multiples of 5,
+ * and FizzBuzz for multiples of both
+ *
+ * Return: always 0
+ */
 int main(void)
 {
-int i;
-for (i = 1; i < 101; i++)
-{
-if (i % 3 == 0 && i % 5 != 0)
-{
-printf("Fizz ");
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 15 == 0)
+			printf("FizzBuzz");
+		else if (i % 3 == 0)
+			printf("Fizz");
+		else if (i % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%i", i);
+		if (i < 100)
+			printf(" ");
+	}
+	printf("\n");
+	return (0);
 }
-else if (i % 3 != 0 && i % 5 == 0)
-{
-printf("Buzz ");
-}
-else if (i % 3 == 0 && i % 5 == 0)
-{
-printf("FizzBuzz ");
-}
-else
-printf("%d ", i);
-}
-printf("\n");
-return (0);
-}
+
