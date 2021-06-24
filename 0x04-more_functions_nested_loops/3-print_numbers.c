54baf3808d095e7ba31e11d7ac4a48f297e47d4d
Eliud-Ngorongo <eliudnjenga@gmail.com>
2021-06-24 15:02:33 +0300

    Lets see

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,17 +1,18 @@
 #include "holberton.h"
+
 /**
- *print_numbers - prints the numbers 0-9 followed by a new line
- *
- *Return: void
- *
- */
+* print_numbers - prints numbers
+*
+* Return: void
+*/
+
 void print_numbers(void)
 {
-int i;
+	int i;
 
-for (i = 0; i <= 9; i++)
-{
-_putchar(i + '0');
-}
-_putchar('\n');
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar(i + '0');
+	}
+	_putchar('\n');
 }
