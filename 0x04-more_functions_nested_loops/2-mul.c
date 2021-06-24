526b1b8e0964ae950dd7eebaf23ea12f44d3dc30
Eliud-Ngorongo <eliudnjenga@gmail.com>
2021-06-24 14:15:49 +0300

    Products(in maths)

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ *mul - multplies intergers a and b
+ * @a: int to be multiplied to b
+ * @b: int to be multiplied to a
+ * Return:gives the product of a and b
+ */
+int mul(int a, int b)
+{
+return (a * b);
+}
