29f6601f5d4bdd417c3758ad7b1a28213b1bc5cc
Eliud-Ngorongo <eliudnjenga@gmail.com>
2021-06-24 14:29:15 +0300

    Printing numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ *print_numbers - prints the numbers 0-9 followed by a new line
+ *
+ *Return: void
+ *
+ */
+void print_numbers(void)
+{
+char n;
+for (n = 0; n < 10; n++)
+{
+_putchar(n);
+}
+_putchar('\n');
+}
