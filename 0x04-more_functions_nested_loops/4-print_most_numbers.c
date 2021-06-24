b3fdeaf5c8bbfd1af8179183c42e1c8923bd5f8d
Eliud-Ngorongo <eliudnjenga@gmail.com>
2021-06-24 15:15:33 +0300

    Stored in an array

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ *print_most_numbers - prints all numbers except 2 and 4
+ *
+ *Return: void
+ */
+void print_most_numbers(void)
+{
+char c[8] ("01356789");
+_putchar(c);
+_putchar('\n');
+}
