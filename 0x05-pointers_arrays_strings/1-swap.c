2c8edada921bc885c9a74e0bb976d530e8dfc6f3
Eliud-Ngorongo <eliudnjenga@gmail.com>
2021-06-28 14:42:27 +0300

    Lets see

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ *swap_int - swaps the value of int a and int b
+ *@a: int to swap to b
+ *@b: int to swap to a
+ *Return: void
+ */
+void swap_int(int *a, int *b)
+{
+
+int x;
+x = *b;
+ *b=*a;
+}
