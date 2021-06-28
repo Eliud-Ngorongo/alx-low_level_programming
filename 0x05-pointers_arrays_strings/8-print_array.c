fbeaa2b101f52f60d639db02848a8cdb0ba8804a
Eliud-Ngorongo <eliudnjenga@gmail.com>
2021-06-28 20:44:49 +0300

    arrays

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -4,12 +4,12 @@
  * print_array - prints intergers stored in an array
  *@a:interger to be printed that is a pointer
  *@n:interger to be printed
- *
+ *	
  */
 void print_array(int *a, int n)
 {
 
-int i = o;
+int i = 0;
 while (i < n)
 {
 printf("%d", a[i]);
