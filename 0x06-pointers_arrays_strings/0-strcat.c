31dbbcd029779154a22497451b7580ae75b73875
Eliud-Ngorongo <eliudnjenga@gmail.com>
2021-07-01 10:39:02 +0300

    Betty compliant

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,13 +1,20 @@
 #include "holberton.h"
+/**
+ * _strcat - appends src to the dest string
+ * @dest: string to append by src
+ * @src : string to append to dest
+ * Return : A pointer to the resulting string
+ */
+
 
 char *_strcat(char *dest, char *src)
 {
-int i ,j ;
+int i, j;
 i = j = 0;
 while (*(dest + i))
- i++;
- while ((*(dest + i)) = (*(src + j)))
- i++;
- j++;
- return (dest);
+i++;
+while ((*(dest + i)) = (*(src + j)))
+i++;
+j++;
+return (dest);
 }
