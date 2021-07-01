e6190478a771ac9d3feeb18f15ef29bf76a4877c
Eliud-Ngorongo <eliudnjenga@gmail.com>
2021-07-01 10:51:19 +0300

    Betty compliant

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,9 +1,11 @@
 #include "holberton.h"
+
 /**
  * _strcat - appends src to the dest string
  * @dest: string to append by src
- * @src : string to append to dest
- * Return : A pointer to the resulting string
+ * @src: string to append to dest
+ *
+ * Return: Pointer of dest
  */
 
 
